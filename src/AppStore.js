0e5a9ae8d84675a04f482c21940035a502cb4af4
masaru-tech <masaru.tech@gmail.com>
2017-03-30 15:55:07 +0900

    チェックイン画面の登録ボタンの表示

--- src/AppStore.js
+++ src/AppStore.js
@@ -1,9 +1,12 @@
-import {extendObservable, observable, action} from 'mobx';
+import {extendObservable, observable, action, computed} from 'mobx';
 
 export default class AppStore {
   constructor(initialStore) {
     extendObservable(this, {
       selectSakes: [], // ArrayっぽいがArrayにあらず
+      get submitBtnDisabled() {
+        return this.selectSakes.length == 0
+      },
 
       pushSelectSake: action((sake) => {
                       this.selectSakes.push({id: sake.id, name: sake.name});
--- src/components/CheckIn.js
+++ src/components/CheckIn.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
-  Dimensions
+  Dimensions,
+  StyleSheet
 } from 'react-native';
-import { List, ListItem } from 'react-native-elements';
+import { List, ListItem, Button } from 'react-native-elements';
 import {observer} from 'mobx-react/native';
+import Colors from '../Colors';
 
-const { height } = Dimensions.get('window');
+const { height, width } = Dimensions.get('window');
 
 export default observer(class CheckIn extends Component {
   constructor(props) {
@@ -26,41 +28,64 @@ export default observer(class CheckIn extends Component {
   }
 
   render() {
-    let selectSakes = this.props.store.selectSakes.map((sake) => {
+    selectSakes = this.props.store.selectSakes.map((sake) => {
       return <ListItem key={sake.id} title={sake.name} hideChevron={true} />
     });
 
     return(
-      <View style={{backgroundColor: '#EEEEEE', height: height}}>
-        <List>
-          <ListItem
-            key='map'
-            title="場所を選択"
-            hideChevron={true}
-            leftIcon={{name: 'md-pin', type: 'ionicon'}}
-          />
-        </List>
-        <List>
-          {selectSakes}
-          <ListItem
-            key='sake'
-            title="お酒を追加"
-            hideChevron={true}
-            leftIcon={{name: 'md-add-circle', type: 'ionicon'}}
-            onPress={() => {
-              this.props.navigator.showModal({
-              screen: 'SakeListScreen',
-              title: "お酒を検索",
-              passProps: {
-                store: this.props.store
-              },
-              navigatorStyle: {},
-              animationType: 'slide-up'
-              });
-            }}
-          />
-        </List>
+      <View>
+        <View style={{backgroundColor: '#EEEEEE', height: height - 19 - 45}}>
+          <List>
+            <ListItem
+              key='map'
+              title="場所を選択"
+              hideChevron={true}
+              leftIcon={{name: 'md-pin', type: 'ionicon'}}
+            />
+          </List>
+          <List>
+            {selectSakes}
+            <ListItem
+              key='sake'
+              title="お酒を追加"
+              hideChevron={true}
+              leftIcon={{name: 'md-add-circle', type: 'ionicon'}}
+              onPress={() => {
+                this.props.navigator.showModal({
+                screen: 'SakeListScreen',
+                title: "お酒を検索",
+                passProps: {
+                  store: this.props.store
+                },
+                navigatorStyle: {},
+                animationType: 'slide-up'
+                });
+              }}
+            />
+          </List>
+        </View>
+
+        <Button
+          buttonStyle={styles.submitBtn}
+          fontWeight='bold'
+          fontSize={20}
+          disabled={this.props.store.submitBtnDisabled}
+          disabledStyle={{backgroundColor: '#C0C0C0'}}
+          title='登録する'
+        />
       </View>
     )
   }
 })
+
+const styles = StyleSheet.create({
+  submitBtn: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    width: width,
+    backgroundColor: Colors.main,
+    marginLeft: 0,
+    marginRight: 0
+  }
+});
