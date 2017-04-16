6f44910b051e1d6b9c9ee81ad5889ea34ba63cb3
masaru-tech <masaru.tech@gmail.com>
2017-04-16 23:01:11 +0900

    react-nativeを0.41.2から0.43.3へアップグレード

--- android/app/src/main/java/com/drankerapp/MainApplication.java
+++ android/app/src/main/java/com/drankerapp/MainApplication.java
@@ -1,7 +1,6 @@
 package com.drankerapp;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.facebook.react.ReactApplication;
 import com.reactnativenavigation.NavigationReactPackage;
